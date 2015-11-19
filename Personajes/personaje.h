c1f54a6607a3156fcf69f7b9da0242f4afd240da
falvarezb <falvarezb@users.noreply.github.com>
2015-11-19 17:52:33 -0500

    arreglado :)

--- Personajes/personaje.h
+++ Personajes/personaje.h
@@ -9,7 +9,7 @@ private:
 	
 public:
 	virtual bool rev_pared(int** map, int movX, int movY) = 0;
-	virtual void movimiento(int movX, int movY) = 0;
+	virtual void void movimiento(bool pared, int movX, int movY) = 0;
 	void getImg(){ return img; }
 	void getIdentificador(){ return identificador; }
 	int getX(){ return x; }
