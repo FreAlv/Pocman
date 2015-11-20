f6b338795c59de1dcf56ee8e612285908ab9c6a8
Fredy Alvarez <fredy.alvarez@ucsp.edu.pe>
2015-11-20 00:46:16 -0500

    cambios en miembros de clase

---
+++ Codigo/personaje.cpp
@@ -0,0 +1,9 @@
+#include "personaje.h"
+
+Personaje::Personaje(int _x, int _y, const char* _img)
+{
+	x = _x;
+	y =_y;
+	img =_img;
+	
+}
--- Codigo/personaje.h
+++ Codigo/personaje.h
@@ -6,15 +6,17 @@ class Personaje
 protected:
 	int x, y;
 	int identificador;
-	char img;
+	const char* img;
 	
 public:
+	Personaje(int _x, int _y, const char* _img);
 	virtual bool rev_pared(int** map, int movX, int movY) = 0;
 	virtual void movimiento(bool pared, int movX, int movY) = 0;
-	char getImg(){ return img; }
+	const char* getImg(){ return img; }
 	int getIdentificador(){ return identificador; }
 	int getX(){ return x; }
 	int getY(){ return y; }
+	
 };
 
 #endif // PERSONAJE_H
