0dbc70b9018760e23171428e07fc65a731a3c134
Juan Velasquez <juan.velasquez@ucsp.edu.pe>
2015-11-23 21:56:36 -0500

    Update fantasma.h
    Arreglo de los fantasmas

--- Codigo/fantasma.h
+++ Codigo/fantasma.h
@@ -1,3 +1,4 @@
+
 #ifndef FANTASMA_H
 #define FANTASMA_H
 
@@ -6,11 +7,13 @@
 
 class Fantasma: public Personaje
 {
+	int PosicionActual;
 public:
 	Fantasma(int x, int y, const char* img, int** map);
 	bool rev_pared(int movX, int movY, int** map);
 	void movimiento( int** map, Pacman pac);
-	
+	int getPosicionActual() { return PosicionActual; }
+	void setPosicionActual(int Valor) { PosicionActual = Valor; }
 };
 
 #endif // FANTASMA_H
