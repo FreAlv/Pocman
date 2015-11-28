77456b2797a822566cc3d0a5657986760a2905fe
juanvm97 <juan.velasquez@ucsp.edu.pe>
2015-11-28 13:03:10 -0500

    q funciona

--- Codigo/main.cpp
+++ Codigo/main.cpp
@@ -14,49 +14,57 @@
 
 using namespace std;
 
-void esperar(Pacman *pacman, Fantasma* F, Tablero *tablero, ControlJuego *control)
+void esperar(int* tecla, Pacman *pacman, Fantasma* F, Tablero *tablero, ControlJuego *control)
 {
-	
-	int tecla = getch();
+
 	while(!tablero->ganar())
 	{	
-		if(tecla == KEY_UP)
+		if(*tecla == KEY_UP)
 		{ 	
 			pacman->movimiento(-1, 0, tablero->getMap(), control);
 		}
 		
-		if(tecla == KEY_DOWN) 
+		if(*tecla == KEY_DOWN) 
 		{	
 			pacman->movimiento(1,0, tablero->getMap(), control);
 		}
 		
-		if(tecla == KEY_LEFT)
+		if(*tecla == KEY_LEFT)
 		{
 			pacman->movimiento(0, -1, tablero->getMap(), control);			
 		}
 		
-		if(tecla == KEY_RIGHT)
+		if(*tecla == KEY_RIGHT)
 		{
 			pacman->movimiento(0, 1, tablero->getMap(), control);
 		}
-		if(tecla == 'q') 
+		if(*tecla == 'q') 
 			break;
 		
 		tablero->print(control->getPantJuego());
-		tecla = getch();
+		*tecla = getch();
 	}
 	
 }
-void juego(Pacman * pacman, Fantasma* F, Tablero *tablero, ControlJuego *control) // La función juego es la función principal
+void juego(Pacman pacman, Fantasma* F, Tablero tablero, ControlJuego* control) // La función juego es la función principal
 {	
 	int tecla;
 
-	thread thread1 (esperar, pacman, F, tablero, control);//esto crea la thread
+	thread thread1 (esperar, &tecla, &pacman, F, &tablero, control);//esto crea la thread
 
 	while(true)
 	{	
-		
-		if (tablero->ganar())
+		if (tecla == 'q') //es para salir
+		{
+			thread1.detach();
+			wclear(control->getPantJuego());
+			wclear(control->getPantDatos());
+			wrefresh(control->getPantJuego());
+			wrefresh(control->getPantDatos());
+			break;
+		}
+
+		if (tablero.ganar())
 		{
 			thread1.detach();//elimina los threads
 			control->ganar();
@@ -68,9 +76,9 @@ void juego(Pacman * pacman, Fantasma* F, Tablero *tablero, ControlJuego *control
 		if (control->getmodoAzul() == false)
 		{
 			for (int i = 0; i < 4; i++)
-				F[i].movimiento(tablero->getMap(), *pacman);
+				F[i].movimiento(tablero.getMap(), pacman);
 			
-			tablero->print(control->getPantJuego());	
+			tablero.print(control->getPantJuego());	
 			usleep(599999); //controla la velicidad de los fantasmas
 		}
 		else
@@ -79,15 +87,14 @@ void juego(Pacman * pacman, Fantasma* F, Tablero *tablero, ControlJuego *control
 			{
 				for (int i = 0; i < 4; i++)
 				{
-					F[i].regresarCaja(tablero->getMap(), *pacman);
-					F[i].mAzul(tablero->getMap(), *pacman);
+					F[i].regresarCaja(tablero.getMap(), pacman);
+					F[i].mAzul(tablero.getMap(), pacman);
 				}
-				tablero->print(control->getPantJuego());
+				tablero.print(control->getPantJuego());
 				usleep(659999);
 			}
 			control->setmodoAzul(false);
 		}
-
 	}
 }
 
@@ -132,23 +139,23 @@ int main()
 	
 	ControlJuego control(1); // le mandas como parámetro el número de niveles que quieres
 	
-	Tablero tablero(mapa);
-	Pacman pacman(16,9, "0<", tablero.getMap());
-	
-	Fantasma* fantasmas;
-	fantasmas = new Fantasma[4];
-	*(fantasmas + 0) = Fantasma(8, 9, "=3", tablero.getMap(), 0);
-	*(fantasmas + 1) = Fantasma(10, 8, "=3", tablero.getMap(), 5);
-	*(fantasmas + 2) = Fantasma(10, 9, "=3", tablero.getMap(), 20);
-	*(fantasmas + 3) = Fantasma(10, 10, "=3", tablero.getMap(), 40);
 	while (true)
 	{
 		menu.usarMenu(control.getPantMenu());
 		if (menu.getOpcion() == 0)
 		{
+			
+			Tablero tablero(mapa);
+			Pacman pacman(16,9, "0<", tablero.getMap());
+			Fantasma* fantasmas;
+			fantasmas = new Fantasma[4];
+			*(fantasmas + 0) = Fantasma(8, 9, "=3", tablero.getMap(), 0);
+			*(fantasmas + 1) = Fantasma(10, 8, "=3", tablero.getMap(), 5);
+			*(fantasmas + 2) = Fantasma(10, 9, "=3", tablero.getMap(), 20);
+			*(fantasmas + 3) = Fantasma(10, 10, "=3", tablero.getMap(), 40);
 			wclear(control.getPantMenu());
 			wrefresh(control.getPantMenu());
-			juego(&pacman, fantasmas, &tablero, &control);
+			juego(pacman, fantasmas, tablero, &control);
 		}
 		if (menu.getOpcion() == 1)
 			mvwprintw(control.getPantMenu(),0,0 , "hola");
